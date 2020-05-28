a51ef84426ad8afe02f208c5c06ded5d121b94cd
joe_chou <joe_chou@ms.usi.com.tw>
2020-05-28 10:29:06 +0800

    first commit

---
+++ com/forfun/ApplicationExample.java
@@ -0,0 +1,43 @@
+package com.forfun;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class ApplicationExample {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ApplicationExample window = new ApplicationExample();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ApplicationExample() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
